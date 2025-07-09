20148bff23dd283ba701f151d89c45978c1f5aac
Guillermo Vidal <guillermo.vidal.astudillo@gmail.com>
2025-07-08 23:27:47 -0300

    menu ejercicio 39

--- 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/39.ejercicio.py
+++ 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/39.ejercicio.py
@@ -1,1 +1,58 @@
-def 
+def inname1():
+    lista=[]
+    while True:
+        try:
+            name=str(input("Ingrese el nombre de pila del estudiante (ingrese x para finalizar): "))
+            if name.isalpha() == True: 
+                if name == "x":
+                     break     
+                lista.append(name)                
+        except ValueError as e:
+            print(f"Ocurrio un error: {e}")
+            
+
+              
+    return      
+
+def menu(option):
+    while True:
+        try:
+            #Menu de opciones
+            print("-- LISTAS DE SECUNDARIA --\n")
+            print("Seleccione una opción: \n"
+                  "1. Ingresar nombres de estudiantes nivel primario\n"
+                  "2. Ingresar nombre de estudiantes nivel secundario\n"
+                  "3. Obtener lista de los estudiantes de nivel primario\n"
+                  "4. Obtener lista de los estudiantes de nivel secundario\n"
+                  "5. Obtener lista de los nombres que se repiten en nivel primario y secundario\n"
+                  "6. Obtener lista de los nombres que NO se repiten en nivel primario y secundario\n"
+                  "7. Salir")
+            #Entrada del usuario
+            option=int(input("Ingrese su opción: ").strip())
+            #Validación de la opción ingresada
+            if option < 1 or option > 7: 
+                raise ValueError("La opción debe ser un número valido entre el 1 y el 7")
+            if not option: 
+                raise ValueError("La opción no puede estar vacía")
+        except ValueError as e:
+            print(f"Ocurrio un error: {e}")
+            print("Ingrese los datos del pasajero de manera correcta")
+        if option == 1:
+                ()
+        elif option == 2:
+                ()
+        elif option == 3:
+                ()
+        elif option == 4:
+                ()
+        elif option == 5: 
+                ()
+        elif option == 6: 
+                ()
+        elif option == 7:
+                ()
+
+#Ejecutar el programa
+if __name__ == "__main__":
+    menu()
+        
