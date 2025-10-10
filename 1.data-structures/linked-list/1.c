bf181188b14d0b6a20944fde0ff98ef010cf2b35
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-10-09 21:37:29 -0300

    1

---
+++ 1.data-structures/linked-list/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int myVal = 13;
+
+    printf("Value of integer 'myVal': %d\n", myVal);
+    printf("Size of integer 'myVal': %lu bytes\n", (unsigned long) sizeof(myVal));// 4 bytes
+    printf("Address to 'myVal': %p\n", (void*)&myVal); //&myVal → 0x7ffca3b8
+    // (void*)&myVal: Convierte esa dirección a puntero genérico para imprimirla correctamente.
+    printf("Size of the address to 'myVal': %lu bytes\n", (unsigned long) sizeof(&myVal)); // 8 bytes
+
+   return 0;
+}
