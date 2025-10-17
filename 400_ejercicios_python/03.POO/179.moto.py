1e53ceb7e6681566f03debdc44fd21b4bc5bd58c
Diadasia chilensis <guillermo.vidal.astudillo@gmail.com>
2025-10-17 07:26:37 -0300

    poo-moto.py

---
+++ 400_ejercicios_python/03.POO/179.moto.py
@@ -0,0 +1,9 @@
+class Moto:
+    def __init__(self,marca,modelo,anho):
+        self.marca  = marca
+        self.modelo = modelo
+        self.anho =   anho
+        
+    def acelerar(self,modelo):
+        
+        
