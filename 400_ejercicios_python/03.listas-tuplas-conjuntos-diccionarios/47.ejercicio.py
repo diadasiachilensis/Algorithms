1ae61c89e9cfdbbf60fdb83b17f7bca0b3cd457f
Diadasia chilensis <guillermo.vidal.astudillo@gmail.com>
2025-10-24 14:05:46 -0300

    exercise 47

--- 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/47.ejercicio.py
+++ 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/47.ejercicio.py
@@ -1,1 +1,10 @@
-
+def del_duplicate(list_bulleye):
+    for i in A:
+        for j in B: 
+            if i == j:
+                print(f"OBJETIVO letra se empieza -> {i}")
+                print(f"BUSQUEDA letra recorrida -> {j}") 
+                j += 1
+                break
+            elif i != j: 
+                print
