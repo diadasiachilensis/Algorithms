34a3d4db0295f3913461963132a5b817f105533a
diadasiachilensis <diadasiachilensis@github.com>
2025-11-09 20:18:55 -0300

    e-58_v-1.1

--- 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/58.ejercicio.py
+++ 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/58.ejercicio.py
@@ -3,4 +3,20 @@
 de un diccionario si existe. 
 """
 def del_key(dic,key):
-    
+    if key in dic:
+        valor=dic.pop(key)
+        print(f"✅ Clave '{key}' ha sido eliminada → Precio: ${dic[key]}")
+    else: 
+        print(f"❌ No se encontró la clave '{key}' en el diccionario.")
+
+frutas = {
+    "manzana": 850,
+    "banana": 700,
+    "naranja": 950,
+    "kiwi": 1200,
+    "uva": 1500
+}
+
+
+del_key(frutas, "kiwi")     # Clave existente
+del_key(frutas, "sandía")   # Clave inexistente
