711f26318c8f67998598bb37f1298ef2ba972a00
diadasiachilensis <diadasiachilensis@github.com>
2025-11-09 19:18:11 -0300

    ejercicio-57

---
+++ 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/57.ejercicio.py
@@ -0,0 +1,14 @@
+"""
+57)Buscar Clave: Escribe una función que busque una clave en un diccionario 
+y devuelva su valor o un mensaje de error si no se encuentra.
+"""
+def search_key(dic,key):
+    for k in dic.keys():
+        if key == dic[key]:
+            print()
+        else:
+
+
+
+
+# end def
