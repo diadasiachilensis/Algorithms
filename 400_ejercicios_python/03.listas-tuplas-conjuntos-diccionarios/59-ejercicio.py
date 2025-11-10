630dd1380eb18044b2c87cf66c85dcda4f15e31f
diadasiachilensis <diadasiachilensis@github.com>
2025-11-09 21:07:09 -0300

    e-59_v-1.0

---
+++ 400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/59-ejercicio.py
@@ -0,0 +1,43 @@
+"""
+59)Diccionario de Contactos: Crea un diccionario de contactos con nombres y números de 
+teléfono.
+"""
+def add_contact(dic):
+    
+    return menu()
+    pass
+
+def edition_contact(dic):
+    
+    return menu()
+    pass
+
+def  del_contact(dic):
+    
+    return menu()
+    pass
+
+def show_contact(dic):
+    
+    return menu()
+    pass
+
+def salir():
+    exit()
+
+
+def menu():
+    while True:
+        opcion = input("""
+========= MENÚ DE CONTACTOS =========
+1. Agregar contacto
+2. Editar contacto
+3. Eliminar contacto
+4. Ver todos los contactos
+5. Buscar contacto
+6. Salir
+====================================
+Seleccione una opción: """)
+        if opcion == 1:
+            agregar_con
+    pass
